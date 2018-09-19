04a9dfbdf92d7597e728c301fb3b88e55fafa926
ChunFeng526 <1248447480@qq.com>
2018-09-19 16:15:57 +0800

    lh

--- config/config.php
+++ config/config.php
@@ -17,7 +17,7 @@ return [
     // 应用命名空间
     'app_namespace'          => 'app',
     // 应用调试模式
-    'app_debug'              => Env::get('app_debug',false),
+    'app_debug'              => Env::get('app_debug',true),
     'app_trace'              => Env::get('app_trace',false),
     // 应用模式状态
     'app_status'             => '',
